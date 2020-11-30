933ce7a90373d464a020ebf2cf0dbfcc653e80d2
phatdroid94 <phatdroid94@gmail.com>
2020-11-30 21:28:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/forcegroundservice05102020/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.forcegroundservice05102020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mBtnStartService;
+    Button mBtnStopService;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnStartService = findViewById(R.id.buttonStartService);
+        mBtnStopService = findViewById(R.id.buttonStopService);
+
+        mBtnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+            }
+        });
+    }
+}
